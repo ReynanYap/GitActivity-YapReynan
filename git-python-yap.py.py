ad95e50fd455d8db6755a1c9bcdecd3e131a3f19
YapReynan <reynanyap25@gmail.com>
2022-03-15 01:09:10 +0800

    My Educational Background added

--- git-python-yap.py.py
+++ git-python-yap.py.py
@@ -1,3 +1,7 @@
 print("Reynan Merjan Yap");
 print("Masiga, Gasan, Marinduque");
 print("February 27, 2001");
+print("Marinduque State College");
+print("BSI/T");
+print("2nd year");
+print("Section C");
